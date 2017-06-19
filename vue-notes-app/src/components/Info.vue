2551a48c75e66c48ca86bf2268dac85b504e7ab5
yrui <yrui@laixue365.com>
2017-06-19 18:10:38 +0800

    实现跨域获得数据

--- vue-notes-app/src/components/Info.vue
+++ vue-notes-app/src/components/Info.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="info">
     <button class="alert" @click="Infomation">alert</button>
+    <div v-for="src in Img">
+      <img v-bind:src="src.src" alt="picture">
+    </div>
   </div>
 </template>
 
@@ -14,9 +17,28 @@
     comfirmIcon: 'fa fa-check-circle'
   })
   export default {
+    data () {
+      return {
+        Img: []
+      }
+    },
+    mounted () {
+      this.getImg()
+    },
     methods: {
       Infomation () {
         notyf.confirm('Please fill out the form')
+      },
+      getImg () {
+        let that = this
+        let url = 'http://localhost:3000/'
+        that.$http.get(url).then((data) => {
+          that.Img = JSON.parse(data.body.message)
+          console.log(that.Img)
+          // for (var i = 0; i < that.Img.length; i++) {
+          //   console.log(that.Img[i])
+          // }
+        })
       }
     }
   }
